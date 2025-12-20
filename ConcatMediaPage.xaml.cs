2df45c13244f2c123309949d21419dbed1b01adf
Peter Egunjobi <masterpdon@gmail.com>
2025-12-20 15:37:48 +0100

    Replace ProcessProgress with ProcessManager control.

--- ConcatMediaPage.xaml.cs
+++ ConcatMediaPage.xaml.cs
@@ -30,10 +30,9 @@ namespace ConcatMediaPage
     public sealed partial class ConcatMediaPage : Page
     {
         private string? navigateTo;
-        private string ffmpegPath;
         private string? outputFile;
         public MainModel viewModel = new() { Items = [] };
-        private readonly ConcatProcessor concatProcessor = new();
+        private ConcatProcessor concatProcessor;
         public static List<string> AllSupportedTypes = [".mkv", ".mp4", ".mp3", ".wav"];
 
         public ConcatMediaPage()
@@ -46,7 +45,7 @@ namespace ConcatMediaPage
             if (e.Parameter is ConcatProps props)
             {
                 navigateTo = props.TypeToNavigateTo;
-                ffmpegPath = props.FfmpegPath;
+                concatProcessor = new ConcatProcessor(props.FfmpegPath);
                 await AddMedia(props.MediaPaths.ToArray());
             }
         }
@@ -106,87 +105,10 @@ namespace ConcatMediaPage
         private async void ProcessConcat(object sender, RoutedEventArgs e)
         {
             if (viewModel.Items.Count < 2) return;
-            ProcessProgress.LeftTextPrimary = string.Empty;
-            ProcessProgress.RightTextPrimary = string.Empty;
-            ProcessProgress.CenterTextSecondary = string.Empty;
-            ProcessProgress.ProgressPrimary = 0;
-            ProcessProgress.ProgressSecondary = 0;
-
             var paths = viewModel.Items.Select(i => i.FilePath).ToArray();
-            viewModel.State = OperationState.DuringOperation;
-
-            var fileProgress = new Progress<FileProgress>(progress =>
-            {
-                if (progress.TotalRangeCount != null) ProcessProgress.LeftTextPrimary = progress.TotalRangeCount;
-                if (progress.CurrentRangeFileName != null)
-                    ProcessProgress.RightTextPrimary = progress.CurrentRangeFileName;
-            });
-            var valueProgress = new Progress<ValueProgress>(progress =>
-            {
-                ProcessProgress.ProgressPrimary = progress.OverallProgress;
-                ProcessProgress.ProgressSecondary = progress.CurrentActionProgress;
-                ProcessProgress.CenterTextSecondary = progress.CurrentActionProgressText;
-            });
-            var failed = false;
-            string? errorMessage = null;
-
-            string? tempOutputFile = null;
             outputFile = null;
-            try
-            {
-                await concatProcessor.Concat(ffmpegPath, paths, fileProgress, valueProgress, SetOutputFile, ErrorActionFromFfmpeg);
-
-                if (viewModel.State == OperationState.BeforeOperation) return; //Canceled
-                if (failed)
-                {
-                    viewModel.State = OperationState.BeforeOperation;
-                    await ErrorAction(errorMessage!);
-                    await concatProcessor.Cancel();
-                    return;
-                }
-
-                viewModel.State = OperationState.AfterOperation;
-                ProcessProgress.RightTextPrimary = "Done";
-                outputFile = tempOutputFile;
-            }
-            catch (Exception ex)
-            {
-                await ErrorAction(ex.Message);
-                viewModel.State = OperationState.BeforeOperation;
-            }
-
-            void ErrorActionFromFfmpeg(string message)
-            {
-                failed = true;
-                errorMessage = message;
-            }
-
-            void SetOutputFile(string file)
-            {
-                tempOutputFile = file;
-            }
-
-            async Task ErrorAction(string message)
-            {
-                ErrorDialog.Title = "Concat operation failed";
-                ErrorDialog.Content = message;
-                await ErrorDialog.ShowAsync();
-            }
+            outputFile = await ProcessManager.StartProcess(concatProcessor.Concat(paths));
         }
-
-        private void ProcessProgress_OnPauseRequested(object? sender, EventArgs e) => concatProcessor.Pause();
-
-        private void ProcessProgress_OnResumeRequested(object? sender, EventArgs e) => concatProcessor.Resume();
-
-        private void ProcessProgress_OnViewRequested(object? sender, EventArgs e) => concatProcessor.ViewFile(outputFile);
-
-        private async void ProcessProgress_OnCancelRequested(object? sender, EventArgs e)
-        {
-            await concatProcessor.Cancel();
-            viewModel.State = OperationState.BeforeOperation;
-        }
-
-        private void ProcessProgress_OnCloseRequested(object? sender, EventArgs e) => viewModel.State = OperationState.BeforeOperation;
 
         private void GoBack(object sender, RoutedEventArgs e)
         {
